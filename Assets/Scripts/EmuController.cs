2f43c1bd088f5956fa3cc4e776543ccd44c2fad2
santoshpavan <santoshpavan.psp@gmail.com>
2021-09-26 12:37:59 -0500

    integrated emu model; needs more work

--- Assets/Scripts/EmuController.cs
+++ Assets/Scripts/EmuController.cs
@@ -11,13 +11,18 @@ public class EmuController : MonoBehaviour {
 
     [SerializeField] private float chaseDistanceThreshold;
     [SerializeField] private float attackDistanceThreshold;
-
     [SerializeField] private Transform playerTransform;
+
     private NavMeshAgent navMeshAgent;
     private State emuState;
+    private Animator animator;
+    
+    private string runState = "Run";
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
         emuState = State.IDLE;
         chaseDistanceThreshold = 15.0f;
         attackDistanceThreshold = 10.0f;
@@ -57,19 +62,27 @@ public class EmuController : MonoBehaviour {
     }
 
     private void emuIdleState() {
-        transform.GetComponent<Renderer>().material.color = Color.blue;
+        // transform.GetComponent<Renderer>().material.color = Color.blue;
+        Debug.Log("Idle");
         navMeshAgent.isStopped = true;
+        animator.SetBool(runState, false);
     }
 
     private void emuChaseState() {
-        transform.GetComponent<Renderer>().material.color = Color.green;
+        // transform.GetComponent<Renderer>().material.color = Color.green;
+        Debug.Log("Chase");
         if (navMeshAgent.isStopped) {
             navMeshAgent.isStopped = false;
         }
+        animator.SetBool(runState, true);
+        // transform.LookAt(playerTransform);
+         Quaternion lookOnLook = Quaternion.LookRotation(playerTransform.position - transform.position);
+ 
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookOnLook, Time.deltaTime);
     }
 
     private void emuAttackState() {
-        transform.GetComponent<Renderer>().material.color = Color.red;
+        // transform.GetComponent<Renderer>().material.color = Color.red;
     }
 
 }
